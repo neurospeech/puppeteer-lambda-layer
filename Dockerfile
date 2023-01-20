72782cd867641a53751d108af98aeb731a4f214f
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-01-20 22:24:34 +0530

    Docker updated

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM public.ecr.aws/lambda/nodejs:18-x86_64
 
+RUN apt-get update \
+    && apt-get install -y wget gnupg chromium mesa-va-drivers libva-drm2 libva-x11-2 mesa-utils mesa-utils-extra \
+    && apt-get update \
+    && apt-get install -y fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 \
+      --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/*
+
 COPY dist ${LAMBDA_TASK_ROOT}/dist
 COPY package.json ${LAMBDA_TASK_ROOT}
 COPY src ${LAMBDA_TASK_ROOT}/src
--- src/SaveUrl.ts
+++ src/SaveUrl.ts
@@ -6,14 +6,12 @@ import { join } from "path";
 // tslint:disable-next-line: max-line-length
 const userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/70.0.3538.75 Mobile/15E148 Safari/605.1";
 
-const executablePath = (() => {
-    const root = join(__dirname, "..", "node_modules", "puppeteer-chromium", "chrome");
-    // get first folder...
-    const first = readdirSync(root)[0];
-    return join(root, first, "chrome-linux", "chrome");
-})();
-
-
+// const executablePath = (() => {
+//     const root = join(__dirname, "..", "node_modules", "puppeteer-chromium", "chrome");
+//     // get first folder...
+//     const first = readdirSync(root)[0];
+//     return join(root, first, "chrome-linux", "chrome");
+// })();
 
 const sleep = (n) => new Promise((resolve, reject) => {
     if (typeof n !== "number") {
@@ -38,11 +36,46 @@ export default class SaveUrl {
 
         const file = await TempFileService.getTempFile("a.png");
 
-        const browser = await puppeteer.launch({
-            ignoreHTTPSErrors: true,
-            executablePath
-        });
-
+        const browser = await puppeteer.launch({executablePath: '/usr/bin/chromium', headless: false, userDataDir: "/tmp",dumpio: true, args: ['--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-dev-shm-usage',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
+        '--disable-setuid-sandbox',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--disk-cache-size=33554432',
+        '--hide-scrollbars',
+        '--ignore-gpu-blacklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--no-pings',
+        '--no-sandbox',
+        '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain',
+        '--single-process',
+        '--enable-logging=stderr',
+        '--v=1',
+        '--disable-gpu']});
         let page = await browser.newPage();
         page.setUserAgent(userAgent);
         page.setViewport({ width, height });
