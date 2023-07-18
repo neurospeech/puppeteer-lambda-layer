0c9a7f76d30cc9adb9599e68b7ce50080b29433b
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-07-18 18:06:47 +0530

    added max height

--- src/commands/GeneratePDF.ts
+++ src/commands/GeneratePDF.ts
@@ -1,12 +1,23 @@
 import { IEvent } from "../IEvent";
 import Command from "./Command";
 
+let document: any;
+
+function getMaxHeight() {
+    return Array.from<any>(document.all).reduce((a, c) => Math.max(c.scrollHeight , a) , 0);
+}
+
 export default class GeneratePDF extends Command {
     async render({ outputFile: path, page, output, pdf }: IEvent) {
+
+        // find div with maximum scroll height
+        const maxHeight = await page.evaluate(getMaxHeight);
         
         const pf = typeof pdf === "object"
             ? { ... pdf, path}
             : { path };
+
+        pf.height = maxHeight + "px";
 
         await page.pdf(pf);
 
