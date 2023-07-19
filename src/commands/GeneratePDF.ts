e6f4ad736c0d3015f6ed3d36a15dd1fcb2cbc7dc
Akash Kava <39438041+ackava@users.noreply.github.com>
2023-07-19 18:03:17 +0530

    Removed print resizing

--- src/commands/GeneratePDF.ts
+++ src/commands/GeneratePDF.ts
@@ -1,36 +1,9 @@
 import { IEvent } from "../IEvent";
 import Command from "./Command";
 
-let document: any;
-
-function getMaxPageSize() {
-    return Array.from<any>(document.all).reduce((a, c) => Math.max(c.scrollHeight , a) , 0);
-}
-
 export default class GeneratePDF extends Command {
     async render({ outputFile: path, page, output, pdf }: IEvent) {
 
-        const height = await page.evaluate(getMaxPageSize);
-
-        const {
-            width,
-            isMobile,
-            deviceScaleFactor,
-            hasTouch,
-            isLandscape
-        } = page.viewport();
-
-        await page.setViewport({
-            width,
-            height,
-            isMobile,
-            deviceScaleFactor,
-            hasTouch,
-            isLandscape
-        });
-        
-        await page.emulateMediaType("screen");
-        
         const pf = typeof pdf === "object"
             ? { ... pdf, path}
             : { path };
